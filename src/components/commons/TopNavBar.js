46243ad08b7529b5aeeec591d8f36840bc1a9904
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-04-02 10:16:30 +0200

    Fixed initial errors

---
+++ src/components/commons/TopNavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const TopNavBar = () => {
+    return(
+        <div className="top_nav_bar_container">
+            Nav Bar
+        </div>
+    )
+}
+
+export default TopNavBar
