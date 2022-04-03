e0c5b946bf4e1dc99259996c66dedc9adac5a446
DrRichadams <36617473+DrRichadams@users.noreply.github.com>
2022-04-03 21:53:22 +0200

    Another commit

--- src/components/A1_Home.js
+++ src/components/A1_Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { CgArrowLongRight, CgArrowLongLeft } from "react-icons/cg"
+import { MdOutlineMyLocation, MdMarkEmailRead, MdCall } from "react-icons/md"
 
 const A1_Home = () =>{
     let banner_message = "Global Procurement Supply and Logistics Centre".split(" ")
@@ -20,6 +21,25 @@ const A1_Home = () =>{
                         <p>APPLY NOW</p>
                         <CgArrowLongLeft size={25} />
                     </button>
+             </div>
+             <div className="home_about_container">
+                 <div className="home_about_texts">
+                     <h3 className="about_home_title">About Us</h3>
+                     <p className="userName">Christine M | MCIPS</p>
+                     <p className="home_about_message">
+                        Global Procurement Supply & Logistics Centre (GPSLC) is a reliable training centre that provides global access to training in procurement and supply operations, supply chain management, logistics management, business management, financial management, office administration, customer service and occupational health and safety at work, enabling companies to develop consistency in knowledge, qualifications and skills and helping to significantly reduce risks throughout supply chains.
+                     </p>
+
+                     <div className="about_home_contacts">
+                         <h4 className="about_home_contacts_title">Reach us and start your awesome journey</h4>
+                         <div><MdOutlineMyLocation size={25} /><p>123 Sam Nujoma Avenue, Windhoek Namibia</p></div>
+                         <div><MdMarkEmailRead size={25} /><p>proxyserver7798@gmail.com</p></div>
+                         <div><MdCall size={25} /><p>+263 813 694 757</p></div>
+                     </div>
+                 </div>
+                 <div className="home_about_img">
+                     <img src={process.env.PUBLIC_URL + `/resources/about_us.png`} alt="" />
+                 </div>
              </div>
         </div>
     )
